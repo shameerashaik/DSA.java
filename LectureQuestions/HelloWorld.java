df622f8a3a3a4d26946b7653cb379263d8a1ebe0
Abbas Khan Mohammad <abbas.khan.mohammadd@gmail.com>
2023-10-04 11:29:19 +0530

    0.3

--- LectureQuestions/HelloWorld.java
+++ LectureQuestions/HelloWorld.java
@@ -3,5 +3,6 @@ package LectureQuestions;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Shammu");
+        System.out.println("Hello Shammu");
     }
 }
